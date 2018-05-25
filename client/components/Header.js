1d2e46da9c3998a10b845695b45bc9dc2ead2cf4
trinhmatthew@gmail.com <trinhmatthew@gmail.com>
2018-05-25 11:34:45 -0400

    Header replaces register and login links with a link to the users profile when the user is logged in

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Header = () => (
+const Header = (props) => (
   <div>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="/">Tidder.</a>
-      <div className='ml-auto'>
-        <Link to='/register'>Register</Link>
-        <Link className='header-login' to='/login'>Login</Link>
-      </div>
+      {
+        props.auth.id ?
+          <div className='ml-auto'>
+            <Link to={`/user/${props.auth.username}`}>{props.auth.username}</Link>
+          </div> :
+          <div className='ml-auto'>
+            <Link to='/register'>Register</Link>
+            <Link className='header-login' to='/login'>Login</Link>
+          </div>
+      }
     </nav>
   </div>
 )
 
-export default Header;
+const mapStateToProps = (state) => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps)(Header);
